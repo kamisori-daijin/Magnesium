1fcbcabc7cc9e94125f122f6bfb9bca02a0d5403
kamisori-daijin <208445078+kamisori-daijin@users.noreply.github.com>
2026-08-13 11:59:05 +0900

    Update Comment

--- Magnesium/Magnesium/ContentView.swift
+++ Magnesium/Magnesium/ContentView.swift
@@ -7,11 +7,11 @@
 
 import SwiftUI
 import MagnesiumKit
-import AppKit // NSOpenPanelを使用するために追加
+import AppKit
 
 struct ContentView: View {
     @State private var renderContext = ANERenderContext()
-    // isImporting の State は不要になったため削除
+   
     
     var body: some View {
         VStack(spacing: 20) {
@@ -32,7 +32,7 @@ struct ContentView: View {
                             Text("Loading Models...")
                         } else {
                             Button("Select 3 .aimodel Files") {
-                                selectFiles() // 関数を呼び出す
+                                selectFiles()
                             }
                             .buttonStyle(.glass)
                         }
@@ -44,17 +44,17 @@ struct ContentView: View {
         }
         .padding()
         .frame(width: 600, height: 680)
-        // .fileImporter は削除しました
+       
     }
     
-    // NSOpenPanelを使ってファイルを選択する関数
+    // Select File
     private func selectFiles() {
         let panel = NSOpenPanel()
         panel.allowsMultipleSelection = true
         panel.canChooseDirectories = false
         panel.canChooseFiles = true
         
-        // ここで拡張子を直接指定
+  
         panel.allowedFileTypes = ["aimodel"]
         
         if panel.runModal() == .OK {
